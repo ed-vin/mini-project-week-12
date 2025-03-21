697d75158cda89626d48fa2d5f4ea6e5481a54df
ed-vin <edvinullberg@outlook.com>
2025-03-21 13:16:18 +0100

    färdig

--- mini-project-week-12/obj/Debug/net9.0/mini-project-week-12.AssemblyInfo.cs
+++ mini-project-week-12/obj/Debug/net9.0/mini-project-week-12.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mini-project-week-12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035c28f43592f44302dfb4cc3c126917dba5d679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac72b2e14ad8d82bbb9dea3e3c72f46ea00671e")]
 [assembly: System.Reflection.AssemblyProductAttribute("mini-project-week-12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mini-project-week-12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
